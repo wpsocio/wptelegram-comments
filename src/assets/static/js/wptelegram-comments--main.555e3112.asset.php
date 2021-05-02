55e9b2813885fdcbfcc92c094f07a59cf88b7130
GitHub Action <github-action@users.noreply.github.com>
2021-05-02 11:55:31 +0000

    Deployed from manzoorwanijk/wp-plugins@1c9104062a5660f2efdd4a01ee87962506ad388f

---
+++ src/assets/static/js/wptelegram-comments--main.555e3112.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => '3ca9222a18d92489756606f95dc78fde');
--- src/assets/static/js/wptelegram-comments--main.f4e26670.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => 'ead847056dc82f0ee6cea4966ddd630a');
