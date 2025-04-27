e9cc3a5f191a0d0358da68c70905c521fb19ad0a
Irshad Ahmad <irshad@wpsocio.com>
2025-04-27 15:17:17 +0000

    Deployed from wpsocio/wp-projects@2def7f75a6b319c34ca8ca36d572bbc2dfe4cad3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpsocio/wptelegram-comments',
-        'pretty_version' => 'dev-b88ac693cbd8bebac153301874233ebdb9e6e018',
-        'version' => 'dev-b88ac693cbd8bebac153301874233ebdb9e6e018',
-        'reference' => 'b88ac693cbd8bebac153301874233ebdb9e6e018',
+        'pretty_version' => 'dev-2def7f75a6b319c34ca8ca36d572bbc2dfe4cad3',
+        'version' => 'dev-2def7f75a6b319c34ca8ca36d572bbc2dfe4cad3',
+        'reference' => '2def7f75a6b319c34ca8ca36d572bbc2dfe4cad3',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../../',
         'aliases' => array(),
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'wpsocio/wptelegram-comments' => array(
-            'pretty_version' => 'dev-b88ac693cbd8bebac153301874233ebdb9e6e018',
-            'version' => 'dev-b88ac693cbd8bebac153301874233ebdb9e6e018',
-            'reference' => 'b88ac693cbd8bebac153301874233ebdb9e6e018',
+            'pretty_version' => 'dev-2def7f75a6b319c34ca8ca36d572bbc2dfe4cad3',
+            'version' => 'dev-2def7f75a6b319c34ca8ca36d572bbc2dfe4cad3',
+            'reference' => '2def7f75a6b319c34ca8ca36d572bbc2dfe4cad3',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../../',
             'aliases' => array(),
